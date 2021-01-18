c990c18c6b69f58ca129d40fcb85cdfc586e6d88
umamahesh33 <63411924+umamahesh33@users.noreply.github.com>
2021-01-18 12:58:08 +0530

    Update Smallest_2ndSmallest.cpp

--- Smallest_2ndSmallest.cpp
+++ Smallest_2ndSmallest.cpp
@@ -1,3 +1,16 @@
+/*Q1- Find the smallest and second smallest elements in an array.
+
+Explanation- Write an efficient C program to find smallest and second smallest element in an array
+
+Example:
+
+Input: arr[] = {12, 13, 1, 10, 34, 1}
+Output: The smallest element is 1 and
+second Smallest element is 10
+*/
+
+
+
 #include <stdio.h> 
 #include <limits.h>
 
